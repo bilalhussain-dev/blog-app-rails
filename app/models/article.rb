62dae416f947fe344b7d79bfd6210e4a875112ac
Bilal Hussain <bilalhussian785@gmail.com>
2022-03-30 13:07:32 +0500

    Creating form and validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 5, maximum: 200}
   validates :description, presence: true, length: {minimum: 5, maximum: 700}
-  
+
 end
